f7afc3b13a9031676a48c06b44b151263ec3fe71
João Pedro Areias de Moraes <joaoareiasmoraes@gmail.com>
2022-11-28 13:52:58 -0300

    Add google analytics

--- src/main.py
+++ src/main.py
@@ -25,6 +25,42 @@ def refresh_volume():
     api_requests.refresh_volume()
 
 
+def inject_ga():
+    import pathlib
+    import shutil
+
+    from bs4 import BeautifulSoup
+    GA_ID = "google_analytics"
+
+    # Note: Please replace the id from G-XXXXXXXXXX to whatever your
+    # web application's id is. You will find this in your Google Analytics account
+    
+    GA_JS = """
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-W6W4DL1THR"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-W6W4DL1THR');
+    </script>
+    """
+
+    # Insert the script in the head tag of the static template inside your virtual
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID):  # if cannot find tag
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  # recover from backup
+        else:
+            shutil.copy(index_path, bck_index)  # keep a backup
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
+
+
 def load_content():
     content = {}
     for path in config.CONTENT_PATH.glob('*.md'):
@@ -87,15 +123,4 @@ filters = {
 data = load_data()
 filtered_data = apply_ui_filters(data, filters)
 ag_grid = format_data(filtered_data)
-
-components.html("""
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-W6W4DL1THR"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-W6W4DL1THR');
-</script>
-""", height=0, width=0)
+inject_ga()
